0dd0d5463ddefd61aad4ec129e3b878e57ede570
myboygroy <68168702+myboygroy@users.noreply.github.com>
2023-05-20 17:03:03 -0400

    Revert "Added JavaDocs to the fibonacci method"

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,11 +1,5 @@
 class Fibonacci {
 
-  /**
-   * Returns the nth term in the Fibonacci sequence
-   * 
-   * @param n   The nth term in the Fibonacci sequence
-   * @return    The value of the term at the nth position of the Fibonacci sequence
-   */
   public static int fibonacci(int n) {
     if (n <= 1)
       return n;
